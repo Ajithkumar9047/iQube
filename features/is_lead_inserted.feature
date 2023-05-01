2993ac905020b5f95e001a264e6c8601565b7298
ajith9047981343 <132073756+ajith9047981343@users.noreply.github.com>
2023-05-01 18:06:11 +0530

    update

--- features/is_lead_inserted.feature
+++ features/is_lead_inserted.feature
@@ -9,33 +9,260 @@ Feature:As a scheduler I want to check the Auto refund, for invalid or missing p
                   "response": "User Id is required"
                   }
                   """
-      Scenario: Should verify the booking is auto Refund passing invalid user id 
-            Given Post the booking with invalid user id
-            When I receive a response
-                  Then I expect the response as
-                  """
-                  {
-                  "message":Booking is not available for this user id."
-                  }
-                  """
-      Scenario: Should verify the booking is auto Refund  with Mismatch user id and Booking id
-            Given Post the booking without Booking id
-            When I receive a response
-                  Then I expect the response as
-                  """
-                  {
-                  "message":"Booking id is mismatched."
-                  }
-                  """
-      # Scenario: Should verify the booking is auto Refund without passing the Transaction id
-      #       Given Post the booking without Transaction id
+      # Scenario: Should verify the booking is auto Refund passing invalid user id 
+      #       Given Post the booking with invalid user id
       #       When I receive a response
       #             Then I expect the response as
       #             """
       #             {
-      #             "message":"'Transaction id' must not be empty."
+      #             "response":Booking is not available "
       #             }
       #             """
+      # Scenario: Should verify the booking is auto Refund  with Mismatch user id and Booking id
+      #       Given Post the booking without Booking id
+      #       When I receive a response
+      #             Then I expect the response as
+      #             """
+      #             {
+      #             "message":"Booking id is mismatched."
+      #             }
+      #             """
+      Scenario: Should verify the booking is auto Refund without passing the Transaction id
+            Given Post the booking without Transaction id
+            When I receive a response
+                  Then I expect the response as
+                  """
+                  {
+                  "response":"Transaction Id is required"
+                  }
+                  """
+      Scenario: Should verify the booking is auto Refund without passing the Payment id 
+            Given Post the booking without Payment id
+            When I receive a response
+                  Then I expect the response as
+                  """
+                  {
+                  "response": "Payment Id is required"
+                  }
+                  """
+      Scenario: Should verify the booking is auto Refund without passing the CancellationInitiatedOn      
+            Given Post the booking without CancellationInitiatedOn
+            When I receive a response
+            Then I expect the response as
+            """
+            {
+            "response": "Cancellation Initiated On is required"
+            }
+            """
+      Scenario: Should verify the booking is auto Refund without passing the Cancellation id 
+            Given Post the booking without Cancellation id
+            When I receive a response
+            Then I expect the response as
+            """
+            {
+            "response": "Cancellation Id is required"
+            }
+            """
+      Scenario: Should verify the booking is auto Refund without passing the ReasonForCancellation  
+            Given Post the booking without ReasonForCancellation
+            When I receive a response
+            Then I expect the response as
+            """
+            {
+            "response": "Reason For Cancellation is required"
+            }
+            """
+
+
+
+      Scenario: Should verify the booking is auto Refund with Mismatch IsCancellation completed and Refund status
+            Given Post the bookingwith Mismatch IsCancellation completed and Refund status
+            When I receive a response
+            Then I expect the response as
+            """
+            {
+            "response": "Invalid Refund Details"
+            }
+            """
+# 11.Scenario: Should verify the booking is auto Refund without passing the Refund status with IsCancellation Completed is True  
+#        Given Post the booking without Refund status with IsCancellation Completed is True
+#        When I receive a response
+#           Then I expect the response as
+#           """
+#           {
+#            “message":"'Refund status' must not be empty."
+#           }
+#           """
+
+      Scenario: Should verify the booking is auto Refund if refund status available but refund  issue date is invalid
+            Given Post the booking with invalid refund date
+            When I receive a response
+            Then I expect the response as
+            """
+            {
+            "response": "Refund Details not available"
+            }
+            """
+
+      Scenario: Should verify the booking is auto Refund if refund status available but Refund _bank_ref_no invalid
+            Given Post the booking with Refund _bank_ref_no invalid
+            When I receive a response
+            Then I expect the response as
+            """
+            {
+            "response": "Refund Details not available"
+            }
+            """
+
+
+
+      Scenario: Should verify the booking is auto Refund if refund status available but refund  amount is invalid
+            Given Post the booking with invalid refund amount
+            When I receive a response
+            Then I expect the response as
+            """
+            {
+            "response": "Refund Details not available"
+            }
+            """
+
+      Scenario: Should verify the booking is auto Refund if refund status available but order amount is invalid
+            Given Post the booking with invalid order amount
+            When I receive a response
+            Then I expect the response as
+            """
+            {
+            "response": "Refund Details not available"
+            }
+            """
+
+      # Scenario: Should verify the booking is auto Refund  when offline booking is true
+      #       Given Post the offline booking is true
+      #       When I receive a response
+      #       Then I expect the response as
+      #       """
+      #       {
+      #       "response": "Unable to cancel, This user Offline Booking "
+      #       }
+      #       """
+      Scenario: Should verify the booking is auto Refund  after Vehicle delivery
+            Given Post the after Vehicle delivery       
+            When I receive a response
+            Then I expect the response as
+            """
+            {
+            "Message":"refund after vehicle delivery"
+            }
+            """
+      Scenario: Should verify the booking is auto Refund  after invoice
+            Given Post the after invoice       
+            When I receive a response
+            Then I expect the response as
+            """
+            {
+            "Message":"refund after vehicle delivery"
+            }
+            """
+      Scenario: Should verify the booking is auto Refund  after FP
+            Given Post the after FP     
+            When I receive a response
+            Then I expect the response as
+            """
+            {
+            "Message":"refund after FP"
+            }
+            """
+# 20 Scenario: Should verify the booking is auto Refund  if duplicate booking
+#         Given Post the duplicate booking       
+#         When I receive a response
+#          Then I expect the response as
+#          """
+#          {
+#          "Message":"your already booking"
+#          }
+#          """
+# 21.Scenario: Should verify the booking is auto Refund without Refund details and Launch city is true 
+#        Given Post the booking without Refund details and Launch city is true 
+#        When I receive a response
+#           Then I expect the response as
+#           """
+#           {
+#            “message":"Refund details is empty & Launch city is true "
+#           }
+#           """
+# 22.Scenario: Should verify the booking is auto Refund without Refund details and Launch city is true 
+#        Given Post the booking without Refund details and Launch city is true 
+#        When I receive a response
+#           Then I expect the response as
+#           """
+#           {
+#            “message":"Refund details is empty & Launch city is true "
+#           }
+#           """
+# 23.Scenario: Should verify the booking is auto Refund with Refund details and Launch city is true 
+#        Given Post the booking with Refund details and Launch city is true 
+#        When I receive a response
+#           Then I expect the response as
+#           """
+#           {
+#            “message":"Refund details & Launch city is true "
+#           }
+#           """
+# 24.Scenario: Should verify the booking is auto Refund without Refund details and Pre Launch city is true 
+#        Given Post the booking without Refund details and Pre Launch city is true 
+#        When I receive a response
+#           Then I expect the response as
+#           """
+#           {
+#            “message":"Refund details is empty & Pre Launch city is true "
+#           }
+#           """
+# 25.Scenario: Should verify the booking is auto Refund with Refund details and Pre Launch city is true 
+#        Given Post the booking with Refund details and Pre Launch city is true 
+#        When I receive a response
+#           Then I expect the response as
+#           """
+#           {
+#            “message":"Refund details & Pre Launch city is true "
+#           }
+#           """
+# 26.Scenario: Should verify the booking is auto Refund without Refund details and ST Launch city is true 
+#        Given Post the booking without Refund details and ST Launch city is true 
+#        When I receive a response
+#           Then I expect the response as
+#           """
+#           {
+#            “message":"Refund details is empty & ST Launch city is true "
+#           }
+#           """
+# 27.Scenario: Should verify the booking is auto Refund with Refund details and ST Launch city is true 
+#        Given Post the booking with Refund details and ST Launch city is true 
+#        When I receive a response
+#           Then I expect the response as
+#           """
+#           {
+#            “message":"Refund details & ST Launch city is true "
+#           }
+#           """
+# 28.Scenario: Should verify the booking is auto Refund without Refund details and ST Pre Launch city is true 
+#        Given Post the booking without Refund details and ST Pre Launch city is true 
+
+#        When I receive a response
+#           Then I expect the response as
+#           """
+#           {
+#            “message":"Refund details is empty & ST Pre Launch city is true "
+#           }
+#           """
+# 29.Scenario: Should verify the booking is auto Refund with Refund details and ST Pre Launch city is true 
+#        Given Post the booking with Refund details and ST Pre Launch city is true 
+#        When I receive a response
+#           Then I expect the response as
+#           """
+#           {
+#            “message":"Refund details & ST Pre Launch city is true "
+#           }
+#           """
 
 
 
--- features/lead.js
+++ features/lead.js
@@ -22,13 +22,359 @@ pactum.stash.addDataTemplate(
             "RefundStatus": null
             }
         }
-
-
 )
 Given('Post the booking without user id', async function () {
     spec["POST".toLowerCase()]("/Cancellation");
     spec.withJson({ '@DATA:TEMPLATE@': "UserId" });    
 });
+
+// pactum.stash.addDataTemplate(
+//     {
+//         invalidUserId:
+//         {
+//         "UserId": 9,
+//         "BookingId": 123,
+//         "CancellationId": "1290412904",
+//         "CancellationInitiatedOn": "2023-04-24T00:00:00",
+//         "PaymentID": 23384,
+//         "RefTransactionId": "IQ-23656",
+//         "TransactionId": "312009438836",
+//         "ReasonForCancellation": "ReasonForCancellation test 312009438836",
+//         "CustomerRemarks": "CustomerRemarks Test 312009438836",
+//         "IsCancellationCompleted": true,
+//         "RefundStatus": null
+//         }
+//     }
+// )
+// Given('Post the booking with invalid user id', async function () {
+// spec["POST".toLowerCase()]("/Cancellation");
+// spec.withJson({ '@DATA:TEMPLATE@': "invalidUserId" });    
+// });
+
+// pactum.stash.addDataTemplate(
+//     {
+//         UserIdmismatch:
+//         {
+//         "UserId":6,
+//         "BookingId": 5600,
+//         "CancellationId": "1290412904",
+//         "CancellationInitiatedOn": "2023-04-24T00:00:00",
+//         "PaymentID": 23384,
+//         "RefTransactionId": "IQ-23656",
+//         "TransactionId": "312009438836",
+//         "ReasonForCancellation": "ReasonForCancellation test 312009438836",
+//         "CustomerRemarks": "CustomerRemarks Test 312009438836",
+//         "IsCancellationCompleted": true,
+//         "RefundStatus": null
+//         }
+//     }
+
+
+// )
+// Given('Post the booking without Booking id', async function () {
+// spec["POST".toLowerCase()]("/Cancellation");
+// spec.withJson({ '@DATA:TEMPLATE@': "UserIdmismatch" });    
+// });
+
+pactum.stash.addDataTemplate(
+    {
+        TranscationId:
+        {
+        "UserId":6,
+        "BookingId": 123,
+        "CancellationId": "1290412904",
+        "CancellationInitiatedOn": "2023-04-24T00:00:00",
+        "PaymentID": 23384,
+        "RefTransactionId": "IQ-23656",
+        "TransactionId": null,
+        "ReasonForCancellation": "ReasonForCancellation test 312009438836",
+        "CustomerRemarks": "CustomerRemarks Test 312009438836",
+        "IsCancellationCompleted": true,
+        "RefundStatus": null
+        }
+    }
+)
+Given('Post the booking without Transaction id', async function () {
+spec["POST".toLowerCase()]("/Cancellation");
+spec.withJson({ '@DATA:TEMPLATE@': "TranscationId" });    
+});
+
+pactum.stash.addDataTemplate(
+    {
+        paymentId:
+        {
+        "UserId": 6,
+        "BookingId": 123,
+        "CancellationId": "1290412904",
+        "CancellationInitiatedOn": "2023-04-24T00:00:00",
+        "PaymentID": null,
+        "RefTransactionId": "IQ-23656",
+        "TransactionId": "312009438836",
+        "ReasonForCancellation": "ReasonForCancellation test 312009438836",
+        "CustomerRemarks": "CustomerRemarks Test 312009438836",
+        "IsCancellationCompleted": true,
+        "RefundStatus": null
+        }
+    }
+)
+Given('Post the booking without Payment id', async function () {
+spec["POST".toLowerCase()]("/Cancellation");
+spec.withJson({ '@DATA:TEMPLATE@': "paymentId" });    
+});
+
+pactum.stash.addDataTemplate(
+    {
+        CancellationInitiatedOn:
+        {
+        "UserId": 6,
+        "BookingId": 123,
+        "CancellationId": "1290412904",
+        "CancellationInitiatedOn": null,
+        "PaymentID": 23384,
+        "RefTransactionId": "IQ-23656",
+        "TransactionId": "312009438836",
+        "ReasonForCancellation": "ReasonForCancellation test 312009438836",
+        "CustomerRemarks": "CustomerRemarks Test 312009438836",
+        "IsCancellationCompleted": true,
+        "RefundStatus": null
+        }
+    }
+)
+Given('Post the booking without CancellationInitiatedOn', async function () {
+spec["POST".toLowerCase()]("/Cancellation");
+spec.withJson({ '@DATA:TEMPLATE@': "CancellationInitiatedOn" });    
+});
+pactum.stash.addDataTemplate(
+    {
+        CancellationId:
+        {
+        "UserId": 6,
+        "BookingId": 123,
+        "CancellationId": null,
+        "CancellationInitiatedOn": "2023-04-24T00:00:00",
+        "PaymentID": 23384,
+        "RefTransactionId": "IQ-23656",
+        "TransactionId": "312009438836",
+        "ReasonForCancellation": "ReasonForCancellation test 312009438836",
+        "CustomerRemarks": "CustomerRemarks Test 312009438836",
+        "IsCancellationCompleted": true,
+        "RefundStatus": null
+        }
+    }
+)
+Given('Post the booking without Cancellation id', async function () {
+spec["POST".toLowerCase()]("/Cancellation");
+spec.withJson({ '@DATA:TEMPLATE@': "CancellationId" });    
+});
+pactum.stash.addDataTemplate(
+    {
+        ReasonForCancellation:
+        {
+        "UserId": 6,
+        "BookingId": 123,
+        "CancellationId": "1290412904",
+        "CancellationInitiatedOn": "2023-04-24T00:00:00",
+        "PaymentID": 23384,
+        "RefTransactionId": "IQ-23656",
+        "TransactionId": "312009438836",
+        "ReasonForCancellation": null,
+        "CustomerRemarks": "CustomerRemarks Test 312009438836",
+        "IsCancellationCompleted": true,
+        "RefundStatus": null
+        }
+    }
+)
+Given('Post the booking without ReasonForCancellation', async function () {
+spec["POST".toLowerCase()]("/Cancellation");
+spec.withJson({ '@DATA:TEMPLATE@': "ReasonForCancellation" });    
+});
+// pactum.stash.addDataTemplate(
+//     {
+//         IsCancellationCompletedmismatch:
+//         {
+//         "UserId": 6,
+//         "BookingId": 123,
+//         "CancellationId": "1290412904",
+//         "CancellationInitiatedOn": "2023-04-24T00:00:00",
+//         "PaymentID": 23384,
+//         "RefTransactionId": "IQ-23656",
+//         "TransactionId": "312009438836",
+//         "ReasonForCancellation": "ReasonForCancellation test 312009438836",
+//         "CustomerRemarks": "CustomerRemarks Test 312009438836",
+//         "IsCancellationCompleted": false,
+//         "RefundStatus": null
+//         }
+//     }
+// )
+// Given('Post the bookingwith Mismatch IsCancellation completed and Refund status', async function () {
+// spec["POST".toLowerCase()]("/Cancellation");
+// spec.withJson({ '@DATA:TEMPLATE@': "IsCancellationCompletedmismatch" });    
+// });
+
+pactum.stash.addDataTemplate(
+    {
+        RefundStatusmismatch:
+        {
+        "UserId": 6,
+        "BookingId": 123,
+        "CancellationId": "1290412904",
+        "CancellationInitiatedOn": "2023-04-24T00:00:00",
+        "PaymentID": 23384,
+        "RefTransactionId": "IQ-23656",
+        "TransactionId": "312009438836",
+        "ReasonForCancellation": "ReasonForCancellation test 312009438836",
+        "CustomerRemarks": "CustomerRemarks Test 312009438836",
+        "IsCancellationCompleted": false,
+        "RefundStatus": {
+        "Refund_issue_date": "2023-04-24T00:00:00",
+        "Refund_bank_ref_no": "312009438836",
+        "Refund_amt": 5000.00,
+        "Order_amt": 5000.00
+    }
+    }
+    }
+)
+Given('Post the bookingwith Mismatch IsCancellation completed and Refund status', async function () {
+spec["POST".toLowerCase()]("/Cancellation");
+spec.withJson({ '@DATA:TEMPLATE@': "RefundStatusmismatch" });    
+});
+pactum.stash.addDataTemplate(
+    {
+        Refund_issue_date:
+        {
+        "UserId": 6,
+        "BookingId": 123,
+        "CancellationId": "1290412904",
+        "CancellationInitiatedOn": "2023-04-24T00:00:00",
+        "PaymentID": 23384,
+        "RefTransactionId": "IQ-23656",
+        "TransactionId": "312009438836",
+        "ReasonForCancellation": "ReasonForCancellation test 312009438836",
+        "CustomerRemarks": "CustomerRemarks Test 312009438836",
+        "IsCancellationCompleted": true,
+        "RefundStatus": {
+        "Refund_issue_date": null,
+        "Refund_bank_ref_no": "312009438836",
+        "Refund_amt": 5000.00,
+        "Order_amt": 5000.00
+    }
+    }
+    }
+)
+Given('Post the booking with invalid refund date', async function () {
+spec["POST".toLowerCase()]("/Cancellation");
+spec.withJson({ '@DATA:TEMPLATE@': "Refund_issue_date" });    
+});
+pactum.stash.addDataTemplate(
+    {
+        Refund_bank_ref_no:
+        {
+        "UserId": 6,
+        "BookingId": 123,
+        "CancellationId": "1290412904",
+        "CancellationInitiatedOn": "2023-04-24T00:00:00",
+        "PaymentID": 23384,
+        "RefTransactionId": "IQ-23656",
+        "TransactionId": "312009438836",
+        "ReasonForCancellation": "ReasonForCancellation test 312009438836",
+        "CustomerRemarks": "CustomerRemarks Test 312009438836",
+        "IsCancellationCompleted": true,
+        "RefundStatus": {
+        "Refund_issue_date": "2023-04-24T00:00:00",
+        "Refund_bank_ref_no": null,
+        "Refund_amt": 5000.00,
+        "Order_amt": 5000.00
+    }
+    }
+    }
+)
+Given('Post the booking with Refund _bank_ref_no invalid', async function () {
+spec["POST".toLowerCase()]("/Cancellation");
+spec.withJson({ '@DATA:TEMPLATE@': "Refund_bank_ref_no" });    
+});
+pactum.stash.addDataTemplate(
+    {
+        Refund_amt:
+        {
+        "UserId": 6,
+        "BookingId": 123,
+        "CancellationId": "1290412904",
+        "CancellationInitiatedOn": "2023-04-24T00:00:00",
+        "PaymentID": 23384,
+        "RefTransactionId": "IQ-23656",
+        "TransactionId": "312009438836",
+        "ReasonForCancellation": "ReasonForCancellation test 312009438836",
+        "CustomerRemarks": "CustomerRemarks Test 312009438836",
+        "IsCancellationCompleted": true,
+        "RefundStatus": {
+        "Refund_issue_date": "2023-04-24T00:00:00",
+        "Refund_bank_ref_no": "312009438836",
+        "Refund_amt": null,
+        "Order_amt": 5000.00
+    }
+    }
+    }
+)
+Given('Post the booking with invalid refund amount', async function () {
+spec["POST".toLowerCase()]("/Cancellation");
+spec.withJson({ '@DATA:TEMPLATE@': "Refund_amt" });    
+});
+pactum.stash.addDataTemplate(
+    {
+        Order_amt:
+        {
+        "UserId": 6,
+        "BookingId": 123,
+        "CancellationId": "1290412904",
+        "CancellationInitiatedOn": "2023-04-24T00:00:00",
+        "PaymentID": 23384,
+        "RefTransactionId": "IQ-23656",
+        "TransactionId": "312009438836",
+        "ReasonForCancellation": "ReasonForCancellation test 312009438836",
+        "CustomerRemarks": "CustomerRemarks Test 312009438836",
+        "IsCancellationCompleted": true,
+        "RefundStatus": {
+        "Refund_issue_date": "2023-04-24T00:00:00",
+        "Refund_bank_ref_no": "312009438836",
+        "Refund_amt": 5000.00,
+        "Order_amt": null
+    }
+    }
+    }
+)
+Given('Post the booking with invalid order amount', async function () {
+spec["POST".toLowerCase()]("/Cancellation");
+spec.withJson({ '@DATA:TEMPLATE@': "Order_amt" });    
+});
+// pactum.stash.addDataTemplate(
+//     {
+//         Useroffline:
+//         {
+//         "UserId": 1,
+//         "BookingId": 123,
+//         "CancellationId": "1290412904",
+//         "CancellationInitiatedOn": "2023-04-24T00:00:00",
+//         "PaymentID": 23384,
+//         "RefTransactionId": "IQ-23656",
+//         "TransactionId": "312009438836",
+//         "ReasonForCancellation": "ReasonForCancellation test 312009438836",
+//         "CustomerRemarks": "CustomerRemarks Test 312009438836",
+//         "IsCancellationCompleted": true,
+//         "RefundStatus": {
+//         "Refund_issue_date": "2023-04-24T00:00:00",
+//         "Refund_bank_ref_no": "312009438836",
+//         "Refund_amt": 5000.00,
+//         "Order_amt": 5000.00
+//     }
+//     }
+//     }
+// )
+// Given('Post the offline booking is true', async function () {
+// spec["POST".toLowerCase()]("/Cancellation");
+// spec.withJson({ '@DATA:TEMPLATE@': "Useroffline" });    
+// });
+
+
 
 
 When('I receive a response', async function () {
